f420c39da0a8a4dc6fac6dbb59a51c7c3eced0ac
bogoquiz <contactsaliens@gmail.com>
2015-03-05 19:03:51 -0500

    socket

--- public/modules/core/services/socket.js
+++ public/modules/core/services/socket.js
@@ -5,7 +5,8 @@ angular.module('core').factory('Socket', ['socketFactory',
     function(socketFactory) {
         return socketFactory({
             prefix: '',
-            ioSocket: io.connect('http://localhost:3000')
+            //ioSocket: io.connect('http://localhost:3000')
+            ioSocket: io.connect('https://shielded-journey-5640.herokuapp.com')
         });
     }
 ]);
